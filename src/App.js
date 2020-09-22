d8a09e725ed165ff48b15537563c3186e7e09bed
Mcbuzzerr <mcbuzzer@gmail.com>
2020-09-22 13:54:14 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Nav from './components/Nav';
+import Yes from './components/Yes';
+
+function App() {
+  return (
+    <>
+      <Nav/>
+      <Yes>John</Yes>
+      <h1 class="App">hello</h1>
+    </>
+  );
+}
+
+export default App;
